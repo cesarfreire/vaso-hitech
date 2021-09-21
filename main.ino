ed53060e4392f8fc373a45da2007b1dd6ec8e89a
César Freire <iceesar@live.com>
2021-09-21 15:34:41 -0300

    Ajustado para 3 botoes

--- main.ino
+++ main.ino
@@ -1,12 +1,27 @@
 // include the library code:
 #include <LiquidCrystal.h>
 
+// Blibioteca sensor temperatura e humidade
+#include "DHT.h"
+ 
+#define DHTPIN A1 // pino que estamos conectando o sensor DTH
+#define DHTTYPE DHT11 // DHT 11 > tipo do DTH
+
+#define sensorHumidade A4
+
+// entrada analógica dos botões
 #define Botoes A0
+
+// Objeto DTH sensor temperatura
+DHT dht(DHTPIN, DHTTYPE);
 
 // --- Protótipo das Funções Auxiliares ---
 void changeMenu(); //Função para modificar o menu atual
 void dispMenu();   //Função para mostrar o menu atual
 void lerBotoes();  //Função que le o estado dos botoes
+
+
+void leTemperatura();
 
 // --- Protótipo das Funções das telas ---
 void tela_inicial();   //Função da tela inicial, data e hora atuais
@@ -30,19 +45,37 @@ byte ultimoMinutoIrrigado = 0; //Variável com valor inicial dos minutos
 // initialize the library with the numbers of the interface pins
 LiquidCrystal disp(8, 9, 4, 5, 6, 7);
 
+float h;
+float t;
+
+// --- Variavel que guarda a humidade do solo
+int humidadeAnalogico;
+int humidade;
+
+// Objeto hora e data
+RTCDateTime dataehora;
+
 void setup()
 {
     pinMode(Botoes, INPUT);
+    pinMode(sensorHumidade, INPUT);
 
     // Inicia o display LCD
     disp.begin(16, 2);
     // Inicia serial
     Serial.begin(9600);
 
+    Serial.println("DHTxx test!");
+
+    // Inicia o DHT
+    dht.begin();
+
     estado_butUP = 0x00; //limpa flag do botão Up
     estado_butDown = 0x00; //limpa flag do botão Down
     estado_butDireito = 0x00; //limpa flag do botão P
     estado_butEsquerdo = 0x00; //limpa flag do botão M
+
+    
 }
 
 void loop()
@@ -50,6 +83,8 @@ void loop()
     lerBotoes();  //Verifica o estado dos botões
     changeMenu(); //Muda a tela conforme a selecionada
     dispMenu();
+    leTemperatura();
+    leHumidadeSolo();
     Serial.println(analogRead(Botoes));
 }
 
@@ -57,37 +92,24 @@ void loop()
 void lerBotoes()
 {
     int valorbotao = analogRead(Botoes);
-    if (valorbotao > 510 && valorbotao < 515)
-        estado_butUP = 1;                //Botão CIMA pressionado? Seta flag
-    if (valorbotao == 0 && estado_butUP) //Botão CIMA solto e flag setada
-    {                                    //Sim...
-        estado_butUP = 0;                //Seta a flag em 0, não apertado
-        funcao_butUp = 1;                //Ativa função Botao CIMA
-    }
-    else if (valorbotao > 255 && valorbotao < 257)
-        estado_butDown = 1;                //Botão BAIXO pressionado? Seta flag
-    if (valorbotao == 0 && estado_butDown) //Botão BAIXO solto e flag setada?
-    {                                      //Se for sim...
-        estado_butDown = 0;                //Limpa flag
-        funcao_butDown = 1;                //Ativa função Botao BAIXO
-    }
-    else if (valorbotao > 340 && valorbotao < 345)
+   
+    if (valorbotao > 1008 && valorbotao < 1020)
         estado_butDireito = 0x01;             //Botão P pressionado? Seta flag
-    if (valorbotao == 0 && estado_butDireito) //Botão P solto e flag setada?                         // BOTÃO LADO DIREITO
+    if (valorbotao < 50 && estado_butDireito) //Botão P solto e flag setada?                         // BOTÃO LADO DIREITO
     {                                         //Se for sim...
         estado_butDireito = 0;                //Limpa flag
         funcao_butDireito = 1;                //Ativa função Botao p
     }
-    else if (valorbotao > 200 && valorbotao < 210)
+    else if (valorbotao > 340 && valorbotao < 372)
         estado_butEsquerdo = 0x01;             //Botão M pressionado? Seta flag
-    if (valorbotao == 0 && estado_butEsquerdo) //Botão M solto e flag setada?                         // BOTÃO LADO ESQUERDO
+    if (valorbotao < 50 && estado_butEsquerdo) //Botão M solto e flag setada?                         // BOTÃO LADO ESQUERDO
     {                                          //Se for sim...
         estado_butEsquerdo = 0;                //Limpa flag
         funcao_butEsquerdo = 1;                //Ativa função Botao M
     }
-    else if (valorbotao > 1020)
+    else if (valorbotao > 500 && valorbotao < 540)
         estado_butSelect = 0x01;             //Botão M pressionado? Seta flag
-    if (valorbotao == 0 && estado_butSelect) //Botão M solto e flag setada?                         // BOTÃO SELECT
+    if (valorbotao < 50 && estado_butSelect) //Botão M solto e flag setada?                         // BOTÃO SELECT
     {                                        //Se for sim...
         estado_butSelect = 0;                //Limpa flag
         funcao_butSelect = 1;                //Ativa função Botao M
@@ -98,8 +120,8 @@ void lerBotoes()
 void changeMenu() //Função para modificar o menu atual
 {
 
-    if (funcao_butUp)
-    {                 //Se a função do botão CIMA foi ATIVA
+    if (funcao_butDireito)
+    {                 //Se a função do botão DIREITO foi ATIVA
         disp.clear(); //Limpa display
         menu++;       //Incrementa o menu
 
@@ -108,7 +130,7 @@ void changeMenu() //Função para modificar o menu atual
         limpaFuncoes();  //Limpa função CIMA
     }                    //end butUp
 
-    if (funcao_butDown)
+    if (funcao_butEsquerdo)
     {                 //Se a função do botão BAIXO foi ATIVA                                                                 //Sim...
         disp.clear(); //Limpa display
         menu--;       //Decrementa o menu
@@ -168,7 +190,7 @@ void telaTemperatura()
     disp.setCursor(2, 0); //Posiciona cursor na coluna 2, linha 1
     disp.print("Temperatura:");
     disp.setCursor(6, 1); //Posiciona cursor na coluna 1, linha 1
-    disp.print("24 C");
+    disp.print(t);
 }
 
 void telaHumidadeSolo()
@@ -176,7 +198,8 @@ void telaHumidadeSolo()
     disp.setCursor(1, 0); //Posiciona cursor na coluna 2, linha 1
     disp.print("Humidade solo:");
     disp.setCursor(6, 1); //Posiciona cursor na coluna 1, linha 1
-    disp.print("XXXX");
+    disp.print(humidade);
+    disp.print(" %");
 }
 
 void telaUltimaIrrigacao()
@@ -213,4 +236,33 @@ void limpaFuncoes()
     funcao_butDireito = 0;
     funcao_butEsquerdo = 0;
     funcao_butSelect = 0;
+}
+
+void leTemperatura()
+{
+
+  h = dht.readHumidity();
+  t = dht.readTemperature();
+  // testa se retorno é valido, caso contrário algo está errado.
+  if (isnan(t) || isnan(h)) 
+  {
+    Serial.println("Failed to read from DHT");
+  } 
+  else
+  {
+    //Serial.print("Umidade: ");
+    //Serial.print(h);
+    //Serial.print(" %t");
+    //Serial.print("Temperatura: ");
+    //Serial.print(t);
+    //Serial.println(" *C");
+  }
+}
+
+void leHumidadeSolo()
+{
+  humidadeAnalogico = analogRead(sensorHumidade);
+  humidade = map(humidadeAnalogico, 1023, 0, 0, 100);
+  //Serial.print("Humidade solo: ");
+  //Serial.println(humidade);
 }
